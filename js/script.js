53bd85ac6192d176bd1133c9eeb6aba4518e22e4
SimoneDUrso <simone.durso98@gmail.com>
2024-06-26 16:01:51 +0200

    [FIX] Modified input text with select

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
-let name_surname = document.getElementById("name-surname");
-let km = document.getElementById("km");
-let age = document.getElementById("age");
+let btn = document.getElementById("calculate-price");
+btn.addEventListener("click", function () {
 
-let sconto = 0;
-let prezzo_biglietto = 0.21 * chilometri;
+    let name_surname = document.getElementById("name-surname").value;
+    let km = document.getElementById("km").value;
+    let age = document.getElementById("age").value;
 
+    let sconto = 0;
+    let prezzo_biglietto = 0.21 * km;
+
+    if (value == "minorenne") {
+        sconto = 0.2
+    }
+
+    else if (value == "senior") {
+        sconto = 0.4
+    }
+
+    let prezzo_finale = prezzo_biglietto - (prezzo_biglietto * sconto);
+
+    document.getElementById("prezzo_finale").innerHTML += prezzo_finale.toFixed(2) + " " + "&euro;"
+    console.log(prezzo_finale);
+})

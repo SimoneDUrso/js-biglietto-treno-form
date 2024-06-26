3cdad52345ade2e2d0aa9956b75202a73e151e6a
SimoneDUrso <simone.durso98@gmail.com>
2024-06-26 15:15:00 +0200

    [UPD] Created form with input text and buttons

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let name_surname = document.getElementById("name-surname");
+let km = document.getElementById("km");
+let age = document.getElementById("age");
+
+let sconto = 0;
+let prezzo_biglietto = 0.21 * chilometri;
+
